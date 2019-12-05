94c7cd0f395eecf57b41e0a09061ed7b81aae3d4
AndKorbiel <andkorbiel@gmail.com>
2019-12-05 22:33:42 +0100

    Next step

--- src/components/actionBox/action-box-container.jsx
+++ src/components/actionBox/action-box-container.jsx
@@ -1,47 +1,61 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import ActionBoxDisplay from './action-box-display'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import ActionBoxDisplay from "./action-box-display";
 
 class ActionBox extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            round: 1,
-        }
-    }
-
-    calcAttack = () => {
-        const { userAttackPoints, } = this.props;
+  constructor(props) {
+    super(props);
+    this.state = {
+      round: 1,
+      attack: 0
     };
+  }
 
-    render() {
-        const { round } = this.state;
-        const { gameMode } = this.props;
-
-        let instruction;
-
-        if (gameMode === "Preconfig") {
-            instruction = "Please configure your Monster"
-        } else {
-            instruction = "Prepare for brawl...!"
-        }
-
-        return (
-            <ActionBoxDisplay gameMode={gameMode} instruction={instruction} round={round} />
-        )
+  calcAttack = () => {
+    const { userAttackPoints } = this.props;
+    function random(min, max) {
+      return Math.floor(Math.random() * (max - min + 1) + min);
     }
+    const randomNumber = random(userAttackPoints / 2, userAttackPoints * 2);
+    this.setState({
+      attack: randomNumber
+    });
+  };
+
+  render() {
+    const { round, attack } = this.state;
+    const { gameMode } = this.props;
+
+    let instruction;
+
+    if (gameMode === "Preconfig") {
+      instruction = "Please configure your Monster";
+    } else {
+      instruction = "Prepare for brawl...!";
+    }
+
+    return (
+      <ActionBoxDisplay
+        gameMode={gameMode}
+        instruction={instruction}
+        round={round}
+        attack={this.calcAttack}
+        displayAttack={attack}
+      />
+    );
+  }
 }
 
-const select = (state) => {
-    return {
-        gameMode: state.gameMode,
-        userAttackPoints: state.monsterConfig.attackPoints,
-        userDefencePoints: state.monsterConfig.defencePoints,
-        userLifePoints: state.monsterConfig.lifePoints,
-        cpuAttackPoints: state.cpuMonsterConfig.attackPoints,
-        cpuDefencePoints: state.cpuMonsterConfig.defencePoints,
-        cpuLifePoints: state.cpuMonsterConfig.lifePoints,
-    }
+const select = state => {
+  return {
+    gameMode: state.gameMode,
+    userAttackPoints: state.monsterConfig.attackPoints,
+    userDefencePoints: state.monsterConfig.defencePoints,
+    userLifePoints: state.monsterConfig.lifePoints,
+    cpuAttackPoints: state.cpuMonsterConfig.attackPoints,
+    cpuDefencePoints: state.cpuMonsterConfig.defencePoints,
+    cpuLifePoints: state.cpuMonsterConfig.lifePoints
+  };
 };
 
-export default connect(select,)(ActionBox);
+export default connect(select)(ActionBox);
--- src/components/actionBox/action-box-display.jsx
+++ src/components/actionBox/action-box-display.jsx
@@ -1,15 +1,17 @@
-import React from 'react';
+import React from "react";
 
-const ActionBoxDisplay =(props)=> {
-    const {gameMode, instruction, round} = props;
+const ActionBoxDisplay = props => {
+  const { gameMode, instruction, round, attack, displayAttack } = props;
 
-    return (
-        <div className="actionBox">
-            <h1>{gameMode}</h1>
-            <h2>{instruction}</h2>
-            <h3>Round: {round}</h3>
-        </div>
-    )
+  return (
+    <div className="actionBox">
+      <h1>{gameMode}</h1>
+      <h2>{instruction}</h2>
+      <h3>Round: {round}</h3>
+      <button onClick={attack}>Attack!</button>
+      <h4>{displayAttack}</h4>
+    </div>
+  );
 };
 
 export default ActionBoxDisplay;

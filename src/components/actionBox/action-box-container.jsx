c7ca79fa6cd4ef2c7e7bf3a88c94f1f71da4c19c
Andrzej Korbiel <andrzej.korbiel@Lumesse.com>
2019-12-05 15:54:14 +0100

    Action stage

--- src/components/actionBox/action-box-container.jsx
+++ src/components/actionBox/action-box-container.jsx
@@ -3,17 +3,44 @@ import { connect } from 'react-redux';
 import ActionBoxDisplay from './action-box-display'
 
 class ActionBox extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            round: 1,
+        }
+    }
+
+    calcAttack = () => {
+        const { userAttackPoints, } = this.props;
+    };
+
     render() {
+        const { round } = this.state;
         const { gameMode } = this.props;
+
+        let instruction;
+
+        if (gameMode === "Preconfig") {
+            instruction = "Please configure your Monster"
+        } else {
+            instruction = "Prepare for brawl...!"
+        }
+
         return (
-            <ActionBoxDisplay gameMode={gameMode} />
+            <ActionBoxDisplay gameMode={gameMode} instruction={instruction} round={round} />
         )
     }
 }
 
 const select = (state) => {
     return {
-        gameMode: state.gameMode
+        gameMode: state.gameMode,
+        userAttackPoints: state.monsterConfig.attackPoints,
+        userDefencePoints: state.monsterConfig.defencePoints,
+        userLifePoints: state.monsterConfig.lifePoints,
+        cpuAttackPoints: state.cpuMonsterConfig.attackPoints,
+        cpuDefencePoints: state.cpuMonsterConfig.defencePoints,
+        cpuLifePoints: state.cpuMonsterConfig.lifePoints,
     }
 };
 
--- src/components/actionBox/action-box-display.jsx
+++ src/components/actionBox/action-box-display.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
 const ActionBoxDisplay =(props)=> {
-    const {gameMode} = props;
+    const {gameMode, instruction, round} = props;
+
     return (
         <div className="actionBox">
             <h1>{gameMode}</h1>
+            <h2>{instruction}</h2>
+            <h3>Round: {round}</h3>
         </div>
     )
 };

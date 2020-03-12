fe849c02397a602f441599de9ff37c1cc1020a7b
Andrzej Korbiel <akorbiel@online.saba.com>
2020-03-12 12:36:22 +0100

    Updates

---
+++ precache-manifest.7953e46f5e2e7f63cf2d2600f00a21bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc1335b26ca9e49142a99f5a10f1c805",
+    "url": "/monster-brawl/index.html"
+  },
+  {
+    "revision": "e62dfa5540a2732f4825",
+    "url": "/monster-brawl/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "32448a712c3758eff6f5",
+    "url": "/monster-brawl/static/css/main.769e093b.chunk.css"
+  },
+  {
+    "revision": "e62dfa5540a2732f4825",
+    "url": "/monster-brawl/static/js/2.502e34c4.chunk.js"
+  },
+  {
+    "revision": "32448a712c3758eff6f5",
+    "url": "/monster-brawl/static/js/main.100721f1.chunk.js"
+  },
+  {
+    "revision": "d3fa3aeddd9bebb182d2",
+    "url": "/monster-brawl/static/js/runtime-main.816dc3f8.js"
+  }
+]);
